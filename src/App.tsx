1922b40cfa009456f71b1c848391029344a35379
Emiliano Sinatra <58446200+emisinatra@users.noreply.github.com>
2023-03-20 17:14:49 -0300

    Añadiendo primera parte del tateti

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
 import { Route, Routes } from "react-router-dom";
 
 import WordPerMinute from "./screens/WordsPerMinute";
-import Pokemon from "./screens/Pokemon";
 import Memotest from "./screens/Memotest";
 import WpmApi from "./screens/WpmApi";
+import Tateti from "./screens/Tateti";
 
 function App() {
   return (
     <Routes>
       <Route element={<Memotest />} path="/memotest" />
-      <Route element={<Pokemon />} path="/pokemon" />
+      <Route element={<Tateti />} path="/tateti" />
       <Route element={<WordPerMinute />} path="/wpm" />
       <Route element={<WpmApi />} path="wpmapi" />
     </Routes>
--- src/screens/Tateti.tsx
+++ src/screens/Tateti.tsx
@@ -1,3 +1,58 @@
+import { useState } from "react";
+
+const WINNING_COMPS = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+];
+
+enum Player {
+  X = "X",
+  O = "O",
+}
+
+enum Status {
+  Jugando = "JUGANDO",
+  Empate = "Empate",
+  XGANO = "X Gano",
+  OGANO = "O Gano",
+}
+
 export default function Tateti() {
-  return <div>{`<Tateti />`}</div>;
+  const [turn, setTurn] = useState<Player>(Player.X);
+  const [cells, setCells] = useState<(Player | "")[]>([
+    Player.X,
+    Player.O,
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+  ]);
+  const [status, setStatus] = useState<Status>(Status.Jugando);
+  function handleClick(index: number) {
+    const draft = [...cells];
+    if (draft[index] == "") {
+      draft[index] = turn;
+    }
+    setTurn((turn) => (turn == Player.X ? Player.O : Player.X));
+    setCells(draft);
+  }
+
+  return (
+    <div className="board">
+      {cells.map((cell, index) => (
+        <div key={index} className="cell" onClick={() => handleClick(index)}>
+          {cell}
+        </div>
+      ))}
+    </div>
+  );
 }
