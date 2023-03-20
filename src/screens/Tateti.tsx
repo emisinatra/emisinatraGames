dd797b4028b545e2756dad339ecce9d8767ee7b9
Emiliano Sinatra <58446200+emisinatra@users.noreply.github.com>
2023-03-20 18:49:24 -0300

    Funcionalidades del juego basicas terminadas

--- src/screens/Tateti.tsx
+++ src/screens/Tateti.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const WINNING_COMPS = [
   [0, 1, 2],
@@ -10,6 +10,7 @@ const WINNING_COMPS = [
   [0, 4, 8],
   [2, 4, 6],
 ];
+const INITIAL_STATE = new Array(9).fill("");
 
 enum Player {
   X = "X",
@@ -25,19 +26,10 @@ enum Status {
 
 export default function Tateti() {
   const [turn, setTurn] = useState<Player>(Player.X);
-  const [cells, setCells] = useState<(Player | "")[]>([
-    Player.X,
-    Player.O,
-    "",
-    "",
-    "",
-    "",
-    "",
-    "",
-    "",
-  ]);
+  const [cells, setCells] = useState<(Player | "")[]>(INITIAL_STATE);
   const [status, setStatus] = useState<Status>(Status.Jugando);
   function handleClick(index: number) {
+    if (status !== Status.Jugando) return;
     const draft = [...cells];
     if (draft[index] == "") {
       draft[index] = turn;
@@ -45,14 +37,51 @@ export default function Tateti() {
     setTurn((turn) => (turn == Player.X ? Player.O : Player.X));
     setCells(draft);
   }
+  function handleReset() {
+    setCells(INITIAL_STATE);
+    setStatus(Status.Jugando);
+  }
+  useEffect(() => {
+    let winner: Player | undefined;
+    for (let player of [Player.X, Player.O]) {
+      const hasWon = WINNING_COMPS.some((comp) =>
+        comp.every((cell) => player == cells[cell])
+      );
+      if (hasWon) {
+        winner = player;
+      }
+      console.log(winner);
+      if (winner == Player.X) {
+        setStatus(Status.XGANO);
+      } else if (winner == Player.O) {
+        setStatus(Status.OGANO);
+      } else if (
+        cells.every((cell) => [Player.O, Player.X].includes(cell as any))
+      ) {
+        setStatus(Status.Empate);
+      }
+    }
+  }, [cells]);
 
   return (
-    <div className="board">
-      {cells.map((cell, index) => (
-        <div key={index} className="cell" onClick={() => handleClick(index)}>
-          {cell}
-        </div>
-      ))}
-    </div>
+    <main>
+      <div className="board">
+        {cells.map((cell, index) => (
+          <div key={index} className="cell" onClick={() => handleClick(index)}>
+            {cell}
+          </div>
+        ))}
+      </div>
+      {status !== Status.Jugando && (
+        <section>
+          <article role="alert">
+            {status == Status.Empate && "Empate!"}
+            {status == Status.XGANO && "Gano X!"}
+            {status == Status.OGANO && "Gano O!"}
+          </article>
+          <button onClick={handleReset}>Reiniciar</button>
+        </section>
+      )}
+    </main>
   );
 }
